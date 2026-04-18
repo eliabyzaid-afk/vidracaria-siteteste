5b70e72b7287a45cd4df3a2804ae2465721a8153
eliabyzaid-afk <eliabyzaid@gmail.com>
2026-04-17 22:37:53 -0300

    Atualiza base path para vidracaria-siteteste

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/vidracaria-site/' : '/',
+  base: mode === 'production' ? '/vidracaria-siteteste/' : '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
